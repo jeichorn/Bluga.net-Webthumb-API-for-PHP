e24f9a9a291c52da1514601457a4010a7189ebd7
Joshua Eichorn <josh@bluga.net>
2009-09-09 09:59:04 -0700

    more renaming

--- examples/Webthumb/usage_debug.php
+++ examples/Webthumb/usage_debug.php
@@ -14,14 +14,14 @@ try {
     // enable debug to see what xml is being sent
     $webthumb->debug = true;
 
-    // you might change the default if one of the PEAR2 adapters is giving you problems
+    // you might change the default if one of the Bluga adapters is giving you problems
     // but its mainly used for testing, the fastest adapter should be choosen by default
     // The 4 Adapters are:
-    // PEAR2_HTTP_Request_Adapter_Http
-    // PEAR2_HTTP_Request_Adapter_Phpstream
-    // PEAR2_HTTP_Request_Adapter_Curl
-    // PEAR2_HTTP_Request_Adapter_Phpsocket
-    //$webthumb->httpRequestAdapter = new PEAR2_HTTP_Request_Adapter_Phpstream();
+    // Bluga_HTTP_Request_Adapter_Http
+    // Bluga_HTTP_Request_Adapter_Phpstream
+    // Bluga_HTTP_Request_Adapter_Curl
+    // Bluga_HTTP_Request_Adapter_Phpsocket
+    //$webthumb->httpRequestAdapter = new Bluga_HTTP_Request_Adapter_Phpstream();
 
     $webthumb->setApiKey($APIKEY);
     $job = $webthumb->addUrl($url,'medium2', 1024, 768);
